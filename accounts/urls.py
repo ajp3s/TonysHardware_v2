0c536f00aeeb1ae42c12a42a669bffa5c8e849ad
Antonio Peres <121125138+ajp3s@users.noreply.github.com>
2023-07-26 21:39:00 +0200

    Added views and templates
    Added the DeleteProfileView, template and url and EditProfileView, template, url and disabled form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.models import User
 from django.urls import path
 
-from TonysHardware_v2.accounts.views import UsersListView, UserCreationView, UserProfileDetailsView, UserLoginView, \
-    UserLogoutView
+from TonysHardware_v2.accounts.views import UserCreationView, UserProfileDetailsView, UserLoginView, \
+    UserLogoutView, UserDeleteProfileView
 
 urlpatterns = [
-    path('', UsersListView.as_view(), name='users_list'),
     path('register/', UserCreationView.as_view(), name='register'),
     path('details/<int:pk>', UserProfileDetailsView.as_view(), name='profile_details'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/<int:pk>', UserLogoutView.as_view(), name='logout'),
+    path('delete/<int:pk>', UserDeleteProfileView.as_view(), name='delete_profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,10 @@
-import boto3
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic as gen_views
 
-from TonysHardware_v2.accounts.forms import BasicUserCreationForm
-from TonysHardware_v2.settings import AWS_STORAGE_BUCKET_NAME
+from TonysHardware_v2.accounts.forms import BasicUserCreationForm, BasicUserEditForm
 
 BasicUserModel = get_user_model()
 
@@ -25,14 +23,14 @@ class UserCreationView(gen_views.CreateView):
         return result
 
 
-class UsersListView(gen_views.ListView):
-    model = BasicUserModel
-
-
 class UserProfileDetailsView(gen_views.DetailView):
     model = BasicUserModel
     template_name = 'accounts/profile_details.html'
     context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        result = super().get_context_data(**kwargs)
+        print(result.get('user').date_joined)
 
 
 class UserLoginView(LoginView):
@@ -45,3 +43,17 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     success_url = reverse_lazy('home page')
+
+
+class UserDeleteProfileView(gen_views.DeleteView):
+    model = BasicUserModel
+    template_name = 'accounts/delete_profile.html'
+    success_url = reverse_lazy('home page')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = self.get_object()
+        context['message'] = 'Do you really want to delete your profile?\n This operation is irreversible.'
+        context['form'] = BasicUserEditForm(instance=obj, disabled=True)
+        return context
+
