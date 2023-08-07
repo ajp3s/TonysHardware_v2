77160e104290832dc0fb36128921bcb312b99bec
Antonio Peres <121125138+ajp3s@users.noreply.github.com>
2023-08-07 22:46:16 +0200

    added custom filters
    created custom filter to extract and iterate trough hardware models field data

---
+++ TonysHardware_v2/hardware/templatetags/custom_filters.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_object_fields_and_values(obj):
+    fields_and_values = []
+    for field in obj._meta.get_fields():
+        field_name, field_value = field.name, getattr(obj, field_name)
+        fields_and_values.append({'name': field_name, 'value': field_value})
+    return fields_and_values
